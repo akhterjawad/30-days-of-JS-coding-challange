56c6bb4257dd0118c23406fc1f9be55f90f00155
akhterjawad <akhterjawad566@gmail.com>
2024-07-13 18:25:18 +0500

    Day 1 Tasks complete

---
+++ Day1; Variable & data types/app.js
@@ -0,0 +1,53 @@
+// Day 1: Variables and Data Types
+// Tasks / Activities:
+// Activity 1: Variable Declaration
+
+// • Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+
+var number = 5;
+console.log(number);
+
+// •Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+
+let string = 'Akhter Jawad';
+console.log(string);
+
+// Activity 2: Constant Declaration
+// •Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+
+const boolean = false;
+console.log(boolean);
+
+// Activity 3: Data Types
+// •Task 4: Create variables of different data types(number, string, boolean, object, array) and log each variable's type using the typeof operator.
+
+let number_1 = 3;
+let string_1 = 'Akhter Jawad'
+let boolean_1 = false;
+let object = {
+    jawad: 'Muhammad Jawad',
+    age: 19
+}
+let array = ['Akhter Jawad', true, 90,]
+console.log(typeof number_1);
+console.log(typeof string_1);
+console.log(typeof boolean_1);
+console.log(typeof object);
+console.log(typeof array);
+
+// Activity 4: Reassigning Variables
+// •Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console.
+
+let main = 'Google';
+console.log(main);
+main = 'Microsoft';
+console.log(main);
+
+// Activity 5: Understanding const
+// •Task 6: Try reassigning a variable declared with const and observe the error.
+
+const master = 'Google';
+master = 'Microsoft';
+console.log(master);//Output: TypeError: Assignment to constant variable.
+
+                           // <!-- Complete --> //
