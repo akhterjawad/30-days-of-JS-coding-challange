3419245e8ded05084034df731704aafb13ea9dd0
akhterjawad <akhterjawad566@gmail.com>
2024-07-18 15:32:40 +0500

    Half above

--- Day5  Function/app.js
+++ Day5  Function/app.js
@@ -19,16 +19,71 @@
 
 // Activity 1: Function Declaration
 // • Task 1: Write a function to check if a number is even or odd and log the result to the console.
+
+let number;
+function check() {
+    number=3
+    let answer = number % 2 === 0 ? `even` : `odd`;
+console.log(answer);
+}
+check();
+
 // • Task 2: Write a function to calculate the square of a number and return the result.
+
+function calculate() {
+    return 3*3;
+}
+console.log(calculate());
+
 // Activity 2: Function Expression
 // • Task 3: Write a function expression to find the maximum of two numbers and log the result to the console.
+let maximun;
+let Expression=function (first,second) {
+    maximun= Math.max(first,second);
+console.log(maximun);
+}
+Expression(8,5);
+
 // • Task 4: Write a function expression to concatenate two strings and return the result.
+
+function concatenate() {
+    let first='Muhammad '
+    let second='Jawad'
+    console.log(first+second);
+}
+concatenate();
+
 // Activity 3: Arrow Functions
 // • Task 5: Write an arrow function to calculate the sum of two numbers and return the result.
+
+let sum =()=>{
+    console.log(3+5);
+}
+sum();
+
 // • Task 6: Write an arrow function to check if a string contains a specific character and return a boolean value.
+
+const containsCharacter = (str, char) => str.includes(char);
+// Example usage:
+console.log(containsCharacter("Hello, world!", "w")); // Outputs: true
+console.log(containsCharacter("Hello, world!", "z")); // Outputs: false
+
+
 // Activity 4: Function Parameters and Default Values
 // • Task 7: Write a function that takes two parameters and returns their product. Provide a default value for the second parameter.
+
+
+
 // • Task 8: Write a function that takes a person's name and age and returns a greeting message. Provide a default value for the age.
+
+
+
 // Activity 5: Higher-Order Functions
 // • Task 9: Write a higher-order function that takes a function and a number, and calls the function that many times.
+
+
+
 // • Task 10: Write a higher-order function that takes two functions and a value, applies the first function to the value, and then applies the second function to the result. 
+
+
+
