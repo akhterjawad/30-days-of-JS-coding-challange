8c883cf4cc852e29a7e19d441898720bfa6a3ae4
akhterjawad <akhterjawad566@gmail.com>
2024-07-20 14:19:24 +0500

    Complete

--- Day6  Array/app.js
+++ Day6  Array/app.js
@@ -16,63 +16,75 @@
 // Activity 1: Array Creation and Access
 // • Task 1: Create an array of numbers from 1 to 5 and log the array to the console.
 
-let numbers= [1,2,3,4,5];
+let numbers = [1, 2, 3, 4, 5];
 console.log(numbers);
 
 // • Task 2: Access the first and last elements of the array and log them to the console.
 
-console.log(`${numbers[0]} & ${numbers[numbers.length-1]}`);
+console.log(`${numbers[0]} & ${numbers[numbers.length - 1]}`);
 
 // Activity 2: Array Methods (Basic)
 // Task 3: Use the push method to add a new number to the end of the array and log the updated array.
 
-let update=numbers.push(49);
+let update = numbers.push(49);
 console.log(numbers);
 
 // • Task 4: Use the pop method to remove the last element from the array and log the updated array.
 
-update=numbers.pop();
+update = numbers.pop();
 console.log(numbers);
 
 // • Task 5: Use the shift method to remove the first element from the array and log the updated array.
 
-update=numbers.shift();
+update = numbers.shift();
 console.log(numbers);
 
 // • Task 6: Use the unshift method to add a new number to the beginning of the array and log the updated array. Activity 3: Array Methods (Intermediate)
 
-update=numbers.unshift(100);
+update = numbers.unshift(100);
 console.log(numbers);
 
 
 // • Task 7: Use the map method to create a new array where each number is doubled and log the new array.
 
+let OriginalArray = [1, 2, 3, 4, 5];
+let doubledNumbers = OriginalArray.map(function (num) {
+    return num * 2;
+});
+console.log(doubledNumbers);
 
 
 // • Task 8: Use the filter method to create a new array with only even numbers and log the new array.
 
-
+const evenNumbers = OriginalArray.filter(number => number % 2 === 0);
+console.log(evenNumbers); // Output: [2, 4, 6, 8, 10]
 
 // • Task 9: Use the reduce method to calculate the sum of all numbers in the array and log the result.
 
-
+main = OriginalArray.reduce((accumelater, second) => accumelater + second, 0);
+console.log(main);
 
 // Activity 4: Array Iteration
 // • Task 10: Use a for loop to iterate over the array and log each element to the console.
 
 for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);   
+    console.log(numbers[i]);
 };
 
 // • Task 11: Use the forEach method to iterate over the array and log each element to the console.
 
-numbers.forEach(item=> console.log(item));
+numbers.forEach(item => console.log(item));
 
 // Activity 5: Multi-dimensional Arrays
 // • Task 12: Create a two-dimensional array (matrix) and log the entire array to the console.
-
+let TwoDimensionalArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+console.log(TwoDimensionalArray);
 
 
 // • Task 13: Access and log a specific element from the two-dimensional array.
 
-
+console.log(TwoDimensionalArray[0][1]);
