351b0f486cae920942e44470411143b6a7bce2a9
Muhammad Jawad <akhterjawad566@gmail.com>
2024-07-23 20:32:30 +0500

    Complete

--- Day8  Es6 features/app.js
+++ Day8  Es6 features/app.js
@@ -32,8 +32,8 @@ console.log(`Hello, my email is ${email} & my password is ${password}`);
 
 // • Task 3: Use array destructuring to extract the first and second elements from an array of numbers and log them to the console.
 
-let fruits=[`watermelon`,`banana`,`mango`,`apple`];
-let [first,second]=fruits;
+let fruits = [`watermelon`, `banana`, `mango`, `apple`];
+let [first, second] = fruits;
 console.log(`${first} ${second}`);
 
 // • Task 4: Use object destructuring to extract the title and author from a book object and log them to the console.
@@ -43,21 +43,21 @@ let book = {
     author: "F. Scott Fitzgerald",
     year: 1925
 };
-let{title,author}=book;
+let { title, author } = book;
 console.log(`title==> ${title}, author==> ${author}`);
 
 // Activity 3: Spread and Rest Operators
 
 // • Task 5: Use the spread operator to create a new array that includes all elements of an existing array plus additional elements, and log the new array to the console.
 
-let Main_Fruits=[`banana`,`mango`,`apple`];
-let array =[`watermelon`,...Main_Fruits]
+let Main_Fruits = [`banana`, `mango`, `apple`];
+let array = [`watermelon`, ...Main_Fruits]
 console.log(array);
 
 // • Task 6: Use the rest operator in a function to accept an arbitrary number of arguments, sum them, and return the result.
 
-let Main_Object=(...username)=>{
-console.log(username);
+let Main_Object = (...username) => {
+    console.log(username);
 }
 Main_Object("abdullah", "usman", "ammar", "zain", "hassan")
 
@@ -65,15 +65,39 @@ Main_Object("abdullah", "usman", "ammar", "zain", "hassan")
 
 // • Task 7: Write a function that takes two parameters and returns their product, with the second parameter having a default value of 1. Log the result of calling this function with and without the second parameter.
 
+function multiply(a, b = 1) {
+    return a * b;
+}
+let result1 = multiply(5, 3);
+console.log(`Result with both parameters: ${result1}`);
+let result2 = multiply(5);
+console.log(`Result with one parameter: ${result2}`);
 
 
 // Activity 5: Enhanced Object Literals
 
 // • Task 8: Use enhanced object literals to create an object with methods and properties, and log the object to the console.
 
+let FirstName ='haseeb';
+let LastName ='ur rehman';
+let value={
+    FirstName,
+    LastName
+ };
+ console.log(value,`FirstName==> ${FirstName} LastName==> ${LastName}`);
 
 
 // • Task 9: Create an object with computed property names based on variables and log the object to the console.
 
+const firstName = 'name';
+const ageKey = 'age';
 
+// Create an object with computed property names
+const person = {
+  [firstName]: 'John',
+  [ageKey]: 30,
+  [' occupation']: 'Developer'
+};
+console.log(person);
 
+//<|=============== COMPLETE =================|>//
