31c4a26de23f8d0f011350b58b4c8990aa71dc01
Muhammad Jawad <akhterjawad566@gmail.com>
2024-07-23 16:43:51 +0500

    70 percent complete

--- Day8  Es6 features/app.js
+++ Day8  Es6 features/app.js
@@ -18,31 +18,48 @@
 
 // • Task 1: Use template literals to create a string that includes variables for a person's name and age, and log the string to the console.
 
-
+const names = "jawad";
+const age = 18;
+console.log(`Hello, my name is ${names} and I am ${age} years old.`);
 
 // • Task 2: Create a multi-line string using template literals and log it to the console.
 
-
+const email = `akhterjawad566@gmail.com`;
+const password = `••••••••••`;
+console.log(`Hello, my email is ${email} & my password is ${password}`);
 
 // Activity 2: Destructuring
 
 // • Task 3: Use array destructuring to extract the first and second elements from an array of numbers and log them to the console.
 
-
+let fruits=[`watermelon`,`banana`,`mango`,`apple`];
+let [first,second]=fruits;
+console.log(`${first} ${second}`);
 
 // • Task 4: Use object destructuring to extract the title and author from a book object and log them to the console.
 
-
+let book = {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925
+};
+let{title,author}=book;
+console.log(`title==> ${title}, author==> ${author}`);
 
 // Activity 3: Spread and Rest Operators
 
 // • Task 5: Use the spread operator to create a new array that includes all elements of an existing array plus additional elements, and log the new array to the console.
 
-
+let Main_Fruits=[`banana`,`mango`,`apple`];
+let array =[`watermelon`,...Main_Fruits]
+console.log(array);
 
 // • Task 6: Use the rest operator in a function to accept an arbitrary number of arguments, sum them, and return the result.
 
-
+let Main_Object=(...username)=>{
+console.log(username);
+}
+Main_Object("abdullah", "usman", "ammar", "zain", "hassan")
 
 // Activity 4: Default Parameters
 
